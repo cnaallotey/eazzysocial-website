9f8c70b407d05e15e09f495199fca02745763d4b
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-06-28 11:55:48 +0000

    add forms on registration

--- src/Views/registercourse.vue
+++ src/Views/registercourse.vue
@@ -40,10 +40,58 @@
               >
                 {{ course.price }}
               </p>
-              <p class="text-base font-normal text-slate-300 leading-6 mt-2">
+              <p
+                class="text-base font-normal text-slate-300 leading-6 mt-2"
+                v-if="!submited"
+              >
                 Partner with us! Fill the form here
               </p>
-              <form action="submit" class="w-full flex flex-col mt-12 0">
+              <div
+                class="w-full space-y-4 flex flex-col items-start mt-12"
+                v-if="submited"
+              >
+                <lottie-player
+                  src="https://assets4.lottiefiles.com/packages/lf20_fbwbq3um.json"
+                  background="transparent"
+                  speed="1"
+                  style="width: 300px; height: 300px"
+                  autoplay
+                ></lottie-player>
+                <p class="text-3xl font-semibold text-white leading-7">
+                  Registration Successful
+                </p>
+                <p class="text-base font-normal text-gray-100 leading-6">
+                  You have registered for {{ course.name }}. Your details have been saved.
+                  Our Team will reach you for confirmation and provide further details on
+                  your training. Hurray on your new Journey 🎉🎉
+                </p>
+                <button
+                  class="w-full flex flex-row items-center justify-center space-x-2 text-base font-normal uppercase tracking-wider bg-blue-600 mt-10 text-white py-3"
+                  v-if="course.brochure != ''"
+                  @click="download(course.brochure)"
+                >
+                  Download Brochure
+                  <span
+                    ><svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5 ml-2 rotate-90"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
+                        clip-rule="evenodd"
+                      /></svg
+                  ></span>
+                </button>
+              </div>
+              <form
+                action="submit"
+                v-if="!submited"
+                @submit.prevent="formsubmit()"
+                class="w-full flex flex-col mt-12 0"
+              >
                 <div
                   class="flex flex-col lg:flex-row lg:space-x-5 space-y-4 lg:space-y-0"
                 >
@@ -55,6 +103,8 @@
                     >
                     <input
                       type="text"
+                      required
+                      v-model="name"
                       class="p-3 bg-white text-base mt-1 font-normal text-slate-800"
                     />
                   </div>
@@ -66,6 +116,8 @@
                 >
                 <input
                   type="email"
+                  required
+                  v-model="email"
                   class="p-3 bg-white text-base mt-1 font-normal text-slate-800"
                 />
 
@@ -76,6 +128,8 @@
                 >
                 <input
                   type="tel"
+                  required
+                  v-model="contact"
                   class="p-3 bg-white text-base mt-1 font-normal text-slate-800"
                 />
                 <label
@@ -90,11 +144,13 @@
                   disabled="true"
                 />
                 <button
+                  type="submit"
                   class="w-full flex flex-row items-center justify-center space-x-2 text-base font-normal uppercase tracking-wider bg-blue-600 mt-10 text-white py-3"
                 >
                   Register
                   <span
                     ><svg
+                      v-if="!loading"
                       xmlns="http://www.w3.org/2000/svg"
                       class="h-5 w-5 ml-2"
                       viewBox="0 0 20 20"
@@ -106,28 +162,48 @@
                         clip-rule="evenodd"
                       /></svg
                   ></span>
+                  <span v-if="loading"
+                    ><svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="#fff"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-loader animate-spin"
+                    >
+                      <line x1="12" y1="2" x2="12" y2="6"></line>
+                      <line x1="12" y1="18" x2="12" y2="22"></line>
+                      <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
+                      <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line>
+                      <line x1="2" y1="12" x2="6" y2="12"></line>
+                      <line x1="18" y1="12" x2="22" y2="12"></line>
+                      <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line>
+                      <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line></svg
+                  ></span>
                 </button>
-                <p
-                  class="text-base mt-5 font-medium text-blue-500 hover:underline select-none cursor-pointer"
-                  v-if="course.brochure != ''"
-                  @click="download(course.brochure)"
-                >
-                  Download Brochure...
-                </p>
               </form>
             </div>
           </div>
         </div>
       </div>
+      <errornotvue v-if="error" />
     </div>
   </div>
 </template>
 
 <script>
 import waves from "../components/waves.vue";
+import errornotVue from "../components/errornot.vue";
+import axios from "axios";
+import Errornot from "../components/errornot.vue";
 export default {
-  components: { waves },
+  components: { waves, errornotVue },
   data() {
+    Errornot;
     return {
       course: [],
       courses: [
@@ -223,11 +299,41 @@ export default {
           brochure: "/brochures/Eazzy_Social_Online_Reputation_Brochure_new.pdf",
         },
       ],
+      loading: false,
+      submited: false,
+      error: false,
+      registrationForm: {},
+      name: "",
+      email: "",
+      contact: "",
     };
   },
   methods: {
     download: function (x) {
       window.open(x, "Download");
+    },
+    formsubmit: function () {
+      this.loading = true;
+      axios
+        .post("https://getform.io/f/0ad3c0c8-9097-4810-9d0c-a4693582d0cf", {
+          name: this.name,
+          email: this.email,
+          contact: this.contact,
+          course: this.course.name,
+        })
+        .then((res) => {
+          console.log(res);
+          this.submited = true;
+          this.loading = false;
+        })
+        .catch((err) => {
+          console.log(err);
+          this.loading = false;
+          this.error = true;
+          setTimeout(() => {
+            this.error = false;
+          }, 3000);
+        });
     },
   },
   created() {
