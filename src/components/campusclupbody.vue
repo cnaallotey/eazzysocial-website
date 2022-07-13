d1e1fdffb5a7cf280d0a356ee44e7848e1193606
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-07-13 19:02:18 +0000

    ui changes

--- src/components/campusclupbody.vue
+++ src/components/campusclupbody.vue
@@ -300,6 +300,19 @@
                                   <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
                                 </svg>
                               </span>
+                              <span v-else
+                                ><svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  class="h-5 w-5"
+                                  viewBox="0 0 20 20"
+                                  fill="currentColor"
+                                >
+                                  <path
+                                    fill-rule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
+                                    clip-rule="evenodd"
+                                  /></svg
+                              ></span>
                             </button>
                           </div>
                         </form>
