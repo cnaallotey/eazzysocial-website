5b51c8fc8b64ec3150f5d8331e6c73c08897eaba
Charles Nii Adotey Allotey <charlesallotey1995@gmail.com>
2022-10-07 07:52:13 +0000

    add country innput

--- src/components/campusclupbody.vue
+++ src/components/campusclupbody.vue
@@ -599,7 +599,7 @@ export default {
           opened: false,
           letter: "Congratsocialmediaadvertising.pdf",
           content:
-            "BLearn how to run social media adverts and start a career as a Social Media Advertising Manager. Drive business performance through ads that convert. Earn stipends while in school. ",
+            "Learn how to run social media adverts and start a career as a Social Media Advertising Manager. Drive business performance through ads that convert. Earn stipends while in school. ",
           img:
             "https://images.unsplash.com/photo-1533895328642-8035bacd565a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
         },
